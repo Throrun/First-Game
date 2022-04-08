dfc2d33fbcefe7a46c882a0a311b63387fcbfd3b
Throrun <47336168+Throrun@users.noreply.github.com>
2022-04-08 22:51:01 +0200

    first commit

---
+++ gracz.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class gracz{
+    private:
+    int HP,LVL,DMG;
+    public:
+    void lvlup();
+    void attack();
+    void getdamage();
+    void death();
+    void setrank();
+};
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "gracz.h"
+
+using namespace std;
+/**
+ * Hello it's my first game and i would try make my first greater project.
+ */
+int main(){
+
+    cout << "Hello World1" << endl;
+    
+    return 0;
+
+}
